79ed9a3aea9d0bc909299b38f290dbf17c1306d3
unknown <cmwillett87@gmail.com>
2021-06-04 16:58:15 -0400

    Defined customer attributes in models.py.  Defined create method in views.py.  Created a create.html and updates the customers\urls.py.  Successfully created a customer with name, address and zip code.

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 # Create your models here.
 
 # TODO: Finish customer model by adding necessary properties to fulfill user stories
@@ -10,5 +11,5 @@ class Customer(models.Model):
     address = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=50)
     balance = models.IntegerField(default=0)
-    suspension_start = models.DateField()
-    suspension_end = models.DateField()
+    suspension_start = models.DateField(default=datetime.date.today())
+    suspension_end = models.DateField(default=datetime.date.today())
--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Customer
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 # TODO: Create a function for each path created in customers/urls.py. Each will need a template as well.
@@ -14,3 +16,15 @@ def index(request):
     # thereby finding the customer/employee profile that matches with the logged-in user.
     print(user)
     return render(request, 'customers/index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        zip_code = request.POST.get('zip_code')
+        new_customer = Customer(name=name, address=address, zip_code=zip_code)
+        new_customer.save()
+        return HttpResponseRedirect(reverse('customers:index'))
+    else:
+        return render(request, 'customers/create.html')
