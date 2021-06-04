5c39223c7c695ea066cc724053aafb844e42e7f9
unknown <cmwillett87@gmail.com>
2021-06-04 14:26:37 -0400

    Moved git folder

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -7,3 +7,8 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+    balance = models.IntegerField(default=0)
+    suspension_start = models.DateField()
+    suspension_end = models.DateField()
