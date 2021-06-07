7fea8a7412ba8758601bcaf8e92dbb1704dee229
lyolayale <lyolayale19@gmail.com>
2021-06-07 10:42:16 -0500

    trying pull

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -14,9 +14,6 @@ class Customer(models.Model):
     address = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=50)
     balance = models.IntegerField(default=0)
-<<<<<<< HEAD
-=======
     one_time_pickup = models.DateField(default=datetime.date.today())
->>>>>>> 8ea5e7b80ea36c2127703799dfbfa3a03d539343
     suspension_start = models.DateField(default=datetime.date.today())
     suspension_end = models.DateField(default=datetime.date.today())
